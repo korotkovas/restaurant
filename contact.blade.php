1d29e2db682982624467ab271ebc5ca6156637b6
Aleksandras <k1999orotkov@mail.ru>
2018-03-08 14:15:13 +0200

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container justify-content-center">
+
+  <div class="row text-center">
+    <div class="col-md-12 order-md-1 text-center">
+    <h1>Gedimino pr. 9, Vilnius</h1>
+    <p>Telefono numeris: +37067575971</p>
+    <p>El.p. info@go9.lt</p>
+    <p>Esame įsikūrę geriausioje sostinės vietoje – Gedimino prospekte. Čia patogu užsukti tiesiog vaikštinėjant miesto gatvėmis, galima atvykti viešuoju transportu, o šiltuoju metų laikotarpiu – miesto dviračiais.
+    Čia pat – požeminė automobilių stovėjimo aikštelė (1 val. – 1.20 Eur).
+    </p>
+    <hr>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5674781.08412197!2d-2.565154393529736!3d46.01340376979494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdb07c903be2a97c8!2s3W+Academy!5e0!3m2!1slt!2slt!4v1514981869035" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+</div>
+
+
+
+@endsection
